9685f5cd840c35b39bb6318e30f6f75a9f15f302
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-06-17 05:03:41 +0900

    Revert the `NINJA_CPP11` macro

--- src/util.h
+++ src/util.h
@@ -51,7 +51,11 @@ NORETURN void Fatal(const char* msg, ...);
 #endif
 
 /// Whether C++11 features are enabled or not.
-#define NINJA_CPP11 __cplusplus >= 201103L || (defined(_MSC_VER) && _MSC_VER >= 1900)
+#if __cplusplus >= 201103L || (defined(_MSC_VER) && _MSC_VER >= 1900)
+#  define NINJA_CPP11 true
+#else
+#  define NINJA_CPP11 false
+#endif
 
 #if defined(__cpp_lib_to_address)
 using std::to_address
